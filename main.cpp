8aacade01fe30895e29ad708bc4005666ae957be
Adrian <cadrian_2001@hotmail.com>
2020-11-11 12:18:33 -0500

    Ahora se lee el codigo desde un archivo

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include "Simpletron.h"
 #include <iostream>
 using namespace std;
-int main(){
-    /*int programa[100];
+#include <fstream>
+using std::ifstream;
+int main(int argc, char *argv[]){
+    int programa[100];
     int n = 0;
-    do{
-        cout << "Ingrese una instruccion > ";
+        //cout << "Ingrese una instruccion > ";
+        ifstream archivoEntrada(argv[1], ios::in);
+
+        if(!archivoEntrada){
+            cerr << "No se pudo abrir el archivo" << endl;
+            return 1;
+        }
         int entrada;
-        cin >> entrada;
-        if(entrada == -1)
-            break;
-        programa[n] = entrada;
-        n++;
-    }while(1);*/
+        while(archivoEntrada >> entrada){
+            programa[n] = entrada;
+            n++;
+        }
+           
     Simpletron st;
     int pNumeroMayor[] = {1009,1010,2009,3110,4107,1109,4300,1110,4300};
     int pcicloSuma[] = {1009,2009,4108,1010,3010,2111,1111,4000,4300};
-    st.cargarPrograma(pcicloSuma,9);
+    st.cargarPrograma(programa,9);
     st.ejecutar();
 }
